bbfcb88af61bdc0222afd1127a21ef800737c1da
VeerMT <71698491+VRmt231@users.noreply.github.com>
2022-02-02 17:04:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function setup(){
     canvas.createCanvas(380,380);
     canvas.center()
@@ -13,6 +14,76 @@ function modelLoaded(){
     status = true;
 }
 
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results
+    }
+}
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('form');
+var inputTxt = document.querySelector('input');
+var voiceSelect = document.querySelector('select');
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for(i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+inputForm.onsubmit = function(event) {
+  event.preventDefault();
+
+  var utterThis = new SpeechSynthesisUtterance("Object Mentioned Found");
+  var selectedOption = voiceSelect.selectedOptions[0].getAttribute('status');
+  for(i = 0; i < voices.length ; i++) {
+    if(voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+  inputTxt.blur();
+}
+
 function draw(){
     image(video, 380, 380)
+    if(status != ""){
+        objectDetector.detect(gotResult)
+       for(i = 0; i < objects.length; i++){
+        document.getElementById("status").innerHTML = "Status - Object Mentioned Found";
+        document.getElementById("number_of_objects").innerHTML = "Number of Objects - " + objects.length;
+
+        fill('#FF0000');
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%"  ,objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke('#FF0000')
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+    }
+    else{
+        var utterThis = new SpeechSynthesisUtterance("Object Mentioned Not Found");
+        synth.speak(utterThis);
+    }
 }
+
